72617b18bf7d8558a58c981225a4e0db019d7147
jvghbbb <130044236+jvghbbb@users.noreply.github.com>
2023-04-06 11:43:58 +0100

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,9 @@
+let messageButton = document.querySelector("#click");
+
+messageButton.addEventListener("click", showMessage);
+
+function showMessage(event)
+{
+    let message="💤😴🤬😡🤪🥵😶🍔🍕";
+    alert(message);
+}
